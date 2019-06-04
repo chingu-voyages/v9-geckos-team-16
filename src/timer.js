685a4fca5a0951e954ce552adf96009019beb586
Angelo I Ryndon <angelo@angeloryndon.com>
2019-06-04 22:53:15 +1200

    Add minutes to seconds function

--- src/timer.js
+++ src/timer.js
@@ -1,4 +1,13 @@
 
-setInterval(() => {
-    console.log(new Date().toUTCString());
-}, 1000);
+
+const minToSeconds = (time) => {
+    return time * 60;
+};
+
+const sessionDuration = minToSeconds(10);
+
+const runTimer = () => {
+    setInterval(() => {
+        console.log(new Date().toUTCString());
+    }, 1000);
+};
