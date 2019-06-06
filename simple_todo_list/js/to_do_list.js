ff97440bf7283f429a055a7a10a721de6c2183ab
Vladimir Peter <vlad.edna@gmail.com>
2019-06-06 18:54:14 +0800

    added a bit of styling, changed crud buttons to clickable images, added feature _check and _uncheck item, re-organized the js/jquery code

--- simple_todo_list/js/to_do_list.js
+++ simple_todo_list/js/to_do_list.js
@@ -1,129 +1,208 @@
 $(document).ready(function(){
 
-	//lists counter
-	var lists_created = 0;
+//global variables
+
+// points to the container where new lists are to be dumped
+var lists_container = $(".lists_container");
+
+// points to the list and list-item html layouts 
+var list_layout_url = "new_list.html";
+var list_item_layout_url = "new_list_item.html" 
 
 
-	var list_item_layout = '<tr><td><input class ="list_item_inputs" type="text"  placeholder="Task"></td><td><button class="add_list_item_btn">Add Task</button></td><td><button class="update_list_item_btn">Update Task</button></td><td><button class="delete_list_item_btn">Delete Task</button></td></tr>';
-
-	
-
-	// points to the container where a new list is to be dumped
-	var lists_container = $(".lists_container");
-	
-	// an input box to enter a new list name 
-	var list_name_input = $("#list_name_input");
-
-
-	// when user clicks create a list 
+/************************* LIST MANIPULATIONS ********************************/
+// when user clicks create list
 	$('#create_list_btn').on("click", function(){
 
-		var new_list_element = '<div id="list' + (lists_created + 1).toString() + '"><input type="text" class="list_name_inputs" id="list_name' +  (lists_created + 1).toString() + '" placeholder="Name your list"> <button class="update_list_btn">Update List</button><button class="delete_list_btn">Delete List</button></div>';
-
-
-		//add a new list element with empty input 
-		lists_container.append(new_list_element);
-		lists_created++; //increment number of lists created 
-
-
-		
-
-
-
-
+		//load a new list onto the lists container
+		 $.get(list_layout_url, function(list_layout, status){
+		 		if(status == "success"){
+		 				lists_container.append(list_layout);
+		 				loadFirstListItem();  // add a single start item
+		 			} 
+		}, "html");
 	});
 
 
+//called to load a new list item to a list that has just been inserted
+function loadFirstListItem(){
 
-/******************  MANIPULATING LISTS ******************************/
+	 $.get(list_item_layout_url, function(list_item, status){
+	 		if(status == "success"){
+					//get the list that has just been inserted 
+					 $(".lists_container .todo_list:last-of-type").append(list_item);
+	 		} 
+	}, "html");
 
-	//when user types new list name 
-	$(".lists_container").on('keyup', ".list_name_inputs", function (e) {
+}
+
+
+// when user types a list name and presses enter 
+lists_container.on('keyup', ".list_name_inputs", function (e) {
+	
 		 // see if user has pressed Enter 
 		    if (e.keyCode == 13) {
 
-		         
-	        var list_number = (this.id).substring(9);
-	        var list_number_id = "#list" + list_number;
-	        var list_name = $("#"+(this.id)).val();
+		    // get the list name 	
+	        var list_name = $(this).val();
 
+	        //	check if the length is valid enough 
 	        if(list_name.length > 3){
-
 	        	//disable the input
-	        	$("#"+(this.id)).attr('disabled','disabled');
+	        	$(this).attr('disabled','disabled');
 
-	        	 //insert  a table for adding tasks to list
-				var check_list_table_layout  = '<table id="tasks_table' + list_number + '">' + list_item_layout +'</table>'; 
-	        	$(list_number_id).append(check_list_table_layout);
-	        }
-	        else{
-	        	$("#"+(this.id)).attr("placeholder","Name is too short").val("");
+	        }else{
+	        	$(this).attr("placeholder","Name is too short").val("");
 	        }
 
 
-		    }
-	});
-
-	//when user clicks update list 
-	$(".lists_container").on("click", ".update_list_btn", function(){
-
-			// get closest input for this button 
-			var list_name_input_to_update = $(this).closest('div').find( ".list_name_inputs" );
-			list_name_input_to_update.removeAttr('disabled');
-
-
-	});
-
-
-	//when user clicks delete list 
-	$(".lists_container").on("click", ".delete_list_btn", function(){
-
-			// get div container for this button 
-			$(this).closest('div').remove();
-
-	});
+		 }
+});
 
 
 
-/******************         MANIPULATING LIST ITEMS ******************************/
-	//when user clicks add new list item to a task 
-	$(".lists_container").on("click", ".add_list_item_btn", function(){
 
-			// get table container for this button 
-			var parent_table = $(this).closest('table');
-			parent_table.append(list_item_layout);
+// when user wishes to update list name  
+lists_container.on('click', ".update_list_img", function(){
 
-	});
+		//enable the list name input that corresponds to this list item
+		$(this).closest('tr').find("input").removeAttr("disabled");
 
-	//when user presses enter on the new list item 
-	$(".lists_container").on('keyup', ".list_item_inputs", function(e){
-			 if (e.keyCode == 13) {
-			 	if($(this).val().length > 3)
-					$(this).attr('disabled','disabled');
-			}
-
-	});
+});
 
 
-	//when user clicks update list item on a task 
-	$(".lists_container").on("click", ".update_list_item_btn", function(){
-
-			// get closest input 
-			var item_input_to_update = $(this).closest('tr').find( "input" );
-			item_input_to_update.removeAttr('disabled');
-
-	});
+// when user wishes to delete a list
+lists_container.on('click', ".delete_list_img", function(){
 
 
-	//when user clicks delete list item from a task 
-	$(".lists_container").on("click", ".delete_list_item_btn", function(){
-
-			// get closest input 
-			var item_input_to_update = $(this).closest('tr').remove();
-
-	});
+		//delete this list
+		$(this).closest('table').remove();
 
 
+});
+
+
+
+
+/************************* LIST ITEMS MANIPULATIONS ********************************/
+//when user wishes to add a new item to list
+lists_container.on('click', ".add_list_item_img", function(){
+
+	var todo_list_to_append_to = $(this).closest('table');
+
+	 $.get(list_item_layout_url, function(list_item, status){
+		 		if(status == "success"){
+		 			//insert a new list item to this table 
+					todo_list_to_append_to.append(list_item);
+		 		} 
+		}, "html");
+			
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// when user types a list item and presses enter 
+lists_container.on('keyup', ".list_item_inputs", function (e) {
+	
+		 // see if user has pressed Enter 
+		    if (e.keyCode == 13) {
+
+		    // get the list item
+	        var list_item = $(this).val();
+
+	        //	check if something has been entered
+	        if(list_item.length > 0){
+	        	//disable the input
+	        	$(this).attr('disabled','disabled');
+
+	        }
+
+
+		 }
+});
+
+
+
+
+// when user wishes to update a list ITEM
+lists_container.on('click', ".update_list_item_img", function(){
+
+		//enable the list name input that corresponds to this list item
+		$(this).closest('tr').find("input").removeAttr("disabled");
+
+});
+
+
+// when user wishes to delete a list item
+lists_container.on('click', ".delete_list_item_img", function(){
+
+		//delete this list item
+		$(this).closest('tr').remove();
+
+
+});
+
+
+// when user indicates is done with a list item
+lists_container.on('click', ".check_item_img", function(){
+
+		//change the image 
+		check_item_img_src = "../images/checked.png";
+		$(this).attr("src", check_item_img_src);
+
+		//change the class of this list item from check to uncheck
+		$(this).removeClass("check_item_img");
+		$(this).addClass("uncheck_item_img");
+
+
+});
+
+
+// when user indicates is not done with a list item
+lists_container.on('click', ".uncheck_item_img", function(){
+
+		//change the image 
+		uncheck_item_img_src = "../images/unchecked.png";
+		$(this).attr("src", uncheck_item_img_src);
+
+		//change the class of this list item from uncheck to check
+		$(this).removeClass("uncheck_item_img");
+		$(this).addClass("check_item_img");
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 
 
 
