717b39ede4b9c574c83918dd1214efe843060cca
Angelo I Ryndon <angelo@angeloryndon.com>
2019-06-06 22:23:33 +1200

    Setup timer. still need to fix logic a bit

--- src/timer.js
+++ src/timer.js
@@ -1,13 +1,91 @@
 
-
-const minToSeconds = (time) => {
-    return time * 60;
+let paused = false;
+let pauseClicked = false;
+let time = {
+    seconds: 5,
+    minutes: 3,
+    timerID: 0
 };
 
-const sessionDuration = minToSeconds(10);
+const setMinutes = (time) => {
+
+    document.getElementById('minutes').innerHTML = time;
+};
+
+const setSeconds = (time) => {
+
+    if (time < 10) {
+
+        document.getElementById('seconds').innerHTML = '0' + time;
+    }
+    else {
+
+        document.getElementById('seconds').innerHTML = time;
+    }
+};
+
+const startPomodoro = () => {
+
+    let button = document.getElementById('start');
+
+    if (paused) {
+
+        button.innerHTML = 'Start';
+        stopTimer();
+        paused = !paused;
+        pauseClicked = true;
+    }
+    else {
+
+        button.innerHTML = 'Pause';
+        runTimer();
+        paused = !paused;
+    }
+};
+
+const stopTimer = () => {
+
+    clearInterval(time.timerID);
+}
 
 const runTimer = () => {
-    setInterval(() => {
-        console.log(new Date().toUTCString());
-    }, 1000);
+
+    console.log(time.minutes);
+    console.log(time.seconds);
+
+    if (time.minutes >= 0) {
+
+        if (!pauseClicked) {
+            --time.minutes;
+        }
+
+        time.timerID = setInterval(() => {
+
+            --time.seconds;
+
+            if (time.minutes >= 0) {
+                setMinutes(time.minutes);
+            }
+
+            if (time.seconds >= 0) {
+                setSeconds(time.seconds);
+            }
+            
+            if (time.seconds <= 0) {
+
+                if (pauseClicked) {
+                    --time.minutes;
+                }
+
+                time.seconds = 5;
+                clearInterval(time.timerID);
+                runTimer();
+            }
+
+        }, 1000);
+    }
 };
+
+//Setup Event-Handlers
+document.getElementById('start')
+    .addEventListener('click', () => startPomodoro());
