2ba5694944756ad9a8b922b262be58b97fb6c3ee
Angelo I Ryndon <angelo@angeloryndon.com>
2019-06-07 11:19:48 +1200

    Address edge-cases on the timer
    When the user clicks Pause multiple times, the seconds clock stops

--- src/timer.js
+++ src/timer.js
@@ -1,42 +1,42 @@
 
 let paused = false;
 let pauseClicked = false;
-let time = {
+let timer = {
+
     seconds: 60,
-    minutes: 0,
+    breakInterval: 0,
+    longBreakMinutes: 0,
+    shortBreakMnutes: 0,
+    sessionMinutes: 0,
     timerID: 0
 };
 
 const setMinutes = () => {
 
-    if (time.minutes >= 0) {
+    if (timer.sessionMinutes >= 0) {
 
-        if (time.minutes == 0) {
+        if (timer.sessionMinutes == 0) {
 
-            document.getElementById('minutes').innerHTML = '0' + time.minutes;
+            document.getElementById('clock-minutes').innerHTML = '0' + timer.sessionMinutes;
         }
         else {
 
-            document.getElementById('minutes').innerHTML = time.minutes;
+            document.getElementById('clock-minutes').innerHTML = timer.sessionMinutes;
         }
     }
 };
 
 const setSeconds = () => {
 
-    if (time.seconds >= 0) {
+    if (timer.seconds >= 0) {
 
-        if (time.seconds == 20) {
+        if (timer.seconds < 10) {
 
-            document.getElementById('seconds').innerHTML = '00';
-        }
-        else if (time.seconds < 10) {
-
-            document.getElementById('seconds').innerHTML = '0' + time.seconds;
+            document.getElementById('clock-seconds').innerHTML = '0' + timer.seconds;
         }
         else {
 
-            document.getElementById('seconds').innerHTML = time.seconds;
+            document.getElementById('clock-seconds').innerHTML = timer.seconds;
         }
     }
 };
@@ -62,38 +62,40 @@ const startPomodoro = () => {
 
 const stopTimer = () => {
 
-    clearInterval(time.timerID);
+    clearInterval(timer.timerID);
 }
 
 const runSecondsTimer = () => {
 
-    time.timerID = setInterval(() => {
+    timer.timerID = setInterval(() => {
 
-        --time.seconds;
+        --timer.seconds;
         setSeconds();
-        
-        if (time.seconds === 0) {
 
-            time.seconds = 12;
-            clearInterval(time.timerID);
-            runTimer();
+        if (timer.seconds === 0) {
+
+            timer.seconds = 60;
+            clearInterval(timer.timerID);
+            if (timer.sessionMinutes >= 0) {
+                runTimer();
+            }
         }
     }, 1000);
 };
 
 const runTimer = () => {
 
-    if (time.minutes > 0 && !pauseClicked) {
+    if (timer.sessionMinutes > 0 && !pauseClicked) {
 
-        --time.minutes;
+        --timer.sessionMinutes;
         setMinutes();
         runSecondsTimer();
     }
-    else if (time.minutes >= 0 && pauseClicked) {
+    else if (pauseClicked) {
 
         setMinutes();
-        --time.minutes;
-       runSecondsTimer();
+        --timer.sessionMinutes;
+        runSecondsTimer();
     }
 };
 
@@ -101,5 +103,27 @@ const runTimer = () => {
 document.getElementById('start')
     .addEventListener('click', () => startPomodoro());
 
-    document.getElementById('user-input')
-    .addEventListener('change',(event)=> time.minutes = event.target.value);
+document.getElementById('timer-input')
+    .addEventListener('change', (event) => {
+
+        let input = event.target;
+
+        if (input.value < 1) {
+            input.value = 1;
+        }
+
+        switch (input.id) {
+            case 'long-break-minutes':
+                timer.longBreakMinutes = input.value;
+                break;
+            case 'short-break-minutes':
+                timer.shortBreakMnutes = input.value;
+                break;
+            case 'session-minutes':
+                timer.sessionMinutes = input.value;
+                break;
+            case 'break-interval':
+                timer.breakInterval = input.value;
+                break;
+        }
+    });
