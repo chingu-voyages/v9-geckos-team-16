2d0ebdd408d4ea849791ebcc0b4e519cba755c27
Simon <simon@learntalk.org>
2019-07-02 18:25:58 +0800

    moved the unsplash bg from <html> to <body>

--- components/appBackground/js/appBackground.js
+++ components/appBackground/js/appBackground.js
@@ -18,7 +18,7 @@ const getBackgroundImage = () => {
     request.addEventListener('load', () => {
 
         let response = JSON.parse(request.response);
-        document.querySelector('html').style.backgroundImage = `url(${response.urls.regular})`;
+        document.querySelector('body').style.backgroundImage = `url(${response.urls.regular})`;
     });
     request.open('get', `https://api.unsplash.com/photos/random/?query=${category}&orientation=landscape`, true);
     request.setRequestHeader('Authorization', 'Client-ID 138d8ad9fa85d98c10db5d138b62b37be74b1120efc7f8649351ef318de6687f');
